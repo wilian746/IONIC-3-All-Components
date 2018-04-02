9da523dda3ff8a1c0ad458ec1073ea74a7ed601a
wilian <wilian746@hotmail.com>
2018-04-02 08:38:07 -0300

    primeiro commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,51 @@
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = HomePage;
+
+  pages: Array<{title: string, component: any}>;
+
+  constructor(
+    public platform: Platform, 
+    public statusBar: StatusBar, 
+    public splashScreen: SplashScreen) {
+    this.initializeApp();
+
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Home', component: HomePage },
+      { title: 'List', component: ListPage },
+      { title: 'Produto', component: 'ProdutoPage'},
+      { title: 'Categoria', component: 'CategoriaPage'}
+    ];
+
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  openPage(page) {
+    if(page.title == "Fechar") {
+      this.platform.exitApp()
+    } else {
+      this.nav.setRoot(page.component)
+    }
+  }
+}
